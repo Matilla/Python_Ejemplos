5d86367dcf04419f444196ddf230b187eb5fe4e4
Administrator <Administrator@172.16.146.137>
2017-03-27 16:37:34 +0200

    First Commit
    Change-Id: Icecb205ec77841e6399296534177599b4a9d8643
    Signed-off-by: Administrator <Administrator@172.16.146.137>

---
+++ Phyton Course/Lesson_1_Control_Flow.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 27, 2017
+
+@author: Administrator
+'''
+import sys
+
+print(sys.byteorder);
+print(sys.api_version);        
+
+if __name__ == '__main__':
+    print('Hello World')
+
