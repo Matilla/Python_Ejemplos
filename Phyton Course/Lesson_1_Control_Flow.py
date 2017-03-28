fbe5fdb0b12210b9ea5a5ea6f92852daf9b7dfe6
Administrator <Administrator@172.16.146.137>
2017-03-28 11:31:53 +0200

    Lesson 1

--- Phyton Course/Lesson_1_Control_Flow.py
+++ Phyton Course/Lesson_1_Control_Flow.py
@@ -3,17 +3,42 @@ Created on Mar 27, 2017
 
 @author: Administrator
 '''
-import sys
 
-print(sys.byteorder);
-print(sys.api_version);        
+# First lesson how to use:
+# Multiple choices - List, Array and Dictionary
+# Flow control - For loop
+# Conditional sentences
 
-if __name__ == '__main__':
-    print('Hello World')
 
-#This is a Comment
-
-print('This is a comment')
-print('This is a second comment')
-print('This is a third comment')
-print('This is a fourth comment')
+def main():
+    
+    print('This is how a Dictionary is use it')
+    choices=dict(
+        alex="A",
+        jane="B",
+        john="C"
+        )
+    print(choices["alex"])
+    
+    print('This is how flow control is use it for FOR sentences')
+    
+    data="This is where I want to break"
+    for char in data:
+        if char=='b': break
+        print(char,end='')
+    
+    print("") # Write a blank line
+    print('This is how conditional sentences is use it for ')
+    
+    x,y=10,1000
+    if (x==10):
+        msg="x is equal to 10"
+    elif (x<y):
+        msg="x is less than y"
+    elif (x==y):
+        msg="x is equal to y"
+    else:
+        msg="x is greater than y"
+    print(msg)
+      
+main()
